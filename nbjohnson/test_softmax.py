b45add98242e8e23ee9877e1c515d41f4638a235
Nicholas <nbjohnson10@wpi.edu>
2020-04-28 20:11:47 -0500

    work on sklearn linear regression

--- nbjohnson/test_softmax.py
+++ nbjohnson/test_softmax.py
@@ -3,6 +3,9 @@ from PIL import Image, ImageOps
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split
+import csv
 
 # pathString = os.path.join("C:", os.sep, "Users", "Nicholas", "Documents", "CS4342", "CS4342_Dog_Breed_Identification", "resized")
 # count = 0
@@ -31,7 +34,55 @@ import matplotlib.pyplot as plt
 # print(X.T.shape)
 # np.save("dog_breed_data.npy", X.T)
 
-data = np.load("dog_breed_data.npy")
+# mydict = {}
+# with open("labels.csv", mode='r') as infile:
+#     reader = csv.reader(infile)
+#     mydict = {rows[0]: rows[1] for rows in reader}
+#
+# print(mydict)
+# count = 0
+# y = 0
+# for image_file in os.listdir(pathString):
+#     if count == 0:
+#         y = np.array([mydict[image_file.split('.')[0]]])
+#         y = np.reshape(y, (-1, 1))
+#     else:
+#         temp = np.array([mydict[image_file.split('.')[0]]])
+#         temp = np.reshape(temp, (-1, 1))
+#         print(temp.shape)
+#         print(temp)
+#         y = np.append(y, temp, axis=0)
+#
+#     count += 1
+#
+# print(y.shape)
+# print(y)
+# np.save("dog_breed_labels.npy", y)
+
+X_in = np.load("dog_breed_data.npy")
 print("data")
-print(data.shape)
-# LR = LogisticRegression(random_state=0).fit(X, y)
+print(X_in.shape)
+
+y_in = np.load("dog_breed_labels.npy")
+print("labels")
+print(y_in.shape)
+
+# X_train = X_in[:8177,:]
+# X_val = X_in[8177:,:]
+# y_train = y_in[:8177,:]
+# y_test = y_in[8177:,:]
+
+X_train, X_test, y_train, y_test = train_test_split(X_in, y_in, test_size=0.2, random_state=42)
+print("done splitting")
+LR = LogisticRegression(random_state=0, verbose=2, n_jobs=4)
+LR.fit(X_train, y_train)
+print("done fitting")
+yhat = LR.predict_proba(X_test)
+print("predicted")
+print(yhat)
+print("actual")
+print(y_test)
+print("done predicting")
+auc = roc_auc_score(y_test, yhat)
+print("scoring")
+print(auc)
