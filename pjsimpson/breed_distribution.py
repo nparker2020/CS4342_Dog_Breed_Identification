e4425b52b43f23efcb0f6a3a57d83e9ed4edd634
psimps21 <pjsimpson@wpi.edu>
2020-05-09 15:08:19 -0400

    Generate bins for top n breeds

--- pjsimpson/breed_distribution.py
+++ pjsimpson/breed_distribution.py
@@ -7,25 +7,34 @@ from PIL import Image
 def convert2DBins(images_df):
     image_dir = '/Users/parkersimpson/PycharmProjects/CS4342/DogBreedProject/dog-breed-identification/resized/'
     for i in range(images_df.shape[0]):
-        if i == 1:
-            break
+        # if i == 2:
+        #     break
 
         label = images_df.breed.iloc[i]
         image_name = images_df.id.iloc[i]
         image = io.imread(image_dir+image_name+'.jpg') # (200,200,3)
 
-        binSize = int(256/32).
-        image_bin = np.zeros((binSize, 1))
-        for j in range(binSize):
-            bin1 = (image[:,:,0] >= (j*binSize)) * (image[:,:,0] < binSize+(j*binSize))
-            count1 = np.sum(bin1)
-            print(count1)
-            # count2 = np.sum(np.nonzero(image[:,:,1] >= (j*binSize) * image[:,:,1] < binSize+(j*binSize)))
-            # count3 = np.sum(np.nonzero(image[:,:,2] >= (j*binSize) * image[:,:,2] < binSize+(j*binSize)))
+        binSize = 32
+        binNum = int(256/binSize)
+        image_bin = np.zeros((binNum, 3))
+        for j in range(binNum):
+            count1 = np.sum((image[:,:,0] >= (j*binSize)) * (image[:,:,0] < binSize+(j*binSize)))
+            count2 = np.sum((image[:,:,1] >= (j*binSize)) * (image[:,:,1] < binSize+(j*binSize)))
+            count3 = np.sum((image[:,:,2] >= (j*binSize)) * (image[:,:,2] < binSize+(j*binSize)))
 
-            image_bin[j,:] = [count1] #, count2, count3]
-        print(np.sum(image_bin))
+            image_bin[j,:] = [count1, count2, count3]
 
+        image_bin = image_bin.ravel()
+        # print(image_bin.shape)
+        if i == 0:
+            X = image_bin[:,None]
+            Y = np.array([label])
+        else:
+            X = np.concatenate((X, image_bin[:,None]), axis=1)
+            Y = np.concatenate((Y, np.array([label])), axis=0)
+
+    print(X.shape, Y.shape)
+    return X, Y
 
 # Load Labels
 labels = pd.read_csv('/Users/parkersimpson/PycharmProjects/CS4342/DogBreedProject/dog-breed-identification/labels.csv')
@@ -41,7 +50,7 @@ gr_labels = gr_labels.sort_values("count", ascending=False)
 n = 2
 for i in range(n):
     breed_i = labels.loc[labels.breed == gr_labels.index[i]]
-    tr_breed_i = breed_i.sample(n=4*breed_i.shape[0]//5) # random 80% of breed
+    tr_breed_i = breed_i.sample(n=4*breed_i.shape[0]//5, random_state=1) # random 80% of breed
     te_breed_i = breed_i[~breed_i.isin(tr_breed_i)].dropna() # random 20% of breed
     # print(tr_breed_i.shape, te_breed_i.shape)
 
@@ -52,6 +61,6 @@ for i in range(n):
         tr_breeds = pd.concat([tr_breeds, tr_breed_i])
         te_breeds = pd.concat([te_breeds, te_breed_i])
 
-# print(tr_breeds.shape, te_breeds.shape)
-# print(tr_breeds.breed.iloc[0])
-convert2DBins(tr_breeds)
+
+trainX, trainY = convert2DBins(tr_breeds)
+testX, testY = convert2DBins(te_breeds)
--- pjsimpson/softmax_binning.py
+++ pjsimpson/softmax_binning.py
@@ -2,6 +2,8 @@ from skimage import io
 import numpy as np
 import os
 
+
+
 # Image bin transformation
 count = 0
 images_dir = '/Users/parkersimpson/PycharmProjects/CS4342/DogBreedProject/dog-breed-identification/resized'
@@ -11,7 +13,7 @@ for image_file in os.listdir(images_dir):
     count += 1
 
     image = io.imread(os.path.join(images_dir, image_file)) # (200,200,3)
-    image = np.reshape(image, (-1,3))
+    image = np.reshape(image, (-1,3)) # (40000, 3)
+    binSize = 200/20
     print(image.shape)
-
 
